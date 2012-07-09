ea0251f4210da1397e11a9813041200df630c10f
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2012-07-09 15:58:03 +0200

    Fixing not existing $PORT variable in unix daemon

--- snippets-server.sh
+++ snippets-server.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-PID_FILE="/tmp/snippets.ariatemplates.com.pid"
+PID_FILE="/tmp/snippets-at.pid"
 SERVER_PATH="$PWD/server.js"
 
 if [ "$1" = "start" ]; then
     if [ -e $PID_FILE ]; then
-        echo "~ snippets.ariatemplates.com daemon is already running."
+        echo "~ snippets-at daemon is already running."
     else
-        echo "~ starting snippets.ariatemplates.com daemon."
-        nohup node $SERVER_PATH $PORT > /dev/null 2>&1 &
+        echo "~ starting snippets-at daemon."
+        nohup node $SERVER_PATH > /dev/null 2>&1 &
     fi
 elif [ "$1" = "stop" ]; then
     if [ ! -e $PID_FILE ]; then
-        echo "~ snippets.ariatemplates.com daemon is not running."
+        echo "~ snippets-at daemon is not running."
     else
-        echo "~ stopping snippets.ariatemplates.com daemon."
+        echo "~ stopping snippets-at daemon."
         kill `cat $PID_FILE`
         rm -f $PID_FILE
     fi
 elif [ "$1" = "status" ]; then
     if [ -e $PID_FILE ]; then
-        echo "~ snippets.ariatemplates.com daemon is running as process "`cat $PID_FILE`
+        echo "~ snippets-at daemon is running as process "`cat $PID_FILE`
     else
-        echo "~ snippets.ariatemplates.com daemon is not running."
+        echo "~ snippets-at daemon is not running."
     fi
 fi
