f122274b6a8354d570a76de211af78ca93e165ff
bcharbonnier <benoit.charbonnier@amadeus.com>
2013-01-15 10:29:58 +0100

    adding the upstart configuration, and removing the fake daemon script

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view options', {
   layout: false
 });
 app.use(app.router);
-app.use(express.static(__dirname + '/public'));
+app.use(express["static"](__dirname + '/public'));
 
 
 app.get('/snippets/github.com/:user/:repo/:file([/\\-._a-zA-Z0-9]+.[a-zA-Z]+)', snippets.onRequest);
@@ -20,11 +20,10 @@ app.get('/samples/github.com/:user/:repo/:folder([/\\-_a-zA-Z0-9]+)', samples.on
 app.get('/code/github.com/:user/:repo/:file([/\\-._a-zA-Z0-9]+.[a-zA-Z]+)', code.onRequest);
 
 
-//process.title ="snippets.ariatemplates.com";
 app.listen(3000);
 console.log("Server %s listening at http://localhost:3000/", process.title);
 
 process.on('uncaughtException', function (err) {
   console.log(err.stack);
+  process.exit(1);
 });
-fs.writeFileSync("/tmp/snippetsd.pid", process.pid.toString() + '\n', 'ascii');
