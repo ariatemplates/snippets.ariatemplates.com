550aae0bc4247723a854dc9270f72c27d3d78a8f
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2013-09-02 17:48:55 +0200

    One commit to rule them all\n\nI could have made it a little bit cleaver with more commit... next time

---
+++ cache.js
@@ -0,0 +1,60 @@
+/**
+ * Cache utility class with First In First Out mecanism
+ */
+var Cache = module.exports = (function() {
+  var DEFAULT_CACHE_SIZE = 100,
+      stack = [],
+      cache = {},
+      debug;
+
+  var log = function() {
+    if (debug) {
+      console.log.apply(console, arguments);
+    }
+  };
+
+  function Cache(options) {
+    debug = (options && options.debug) || false;
+    this.max_size = (options && options.size) || DEFAULT_CACHE_SIZE;
+    log("[Cache]", "Initialized with size:", this.max_size);
+  }
+
+  Cache.prototype = {
+    put: function(key, item) {
+      var to_remove;
+      if (this.size() > this.max_size) {
+        to_remove = stack.shift();
+        log("[Cache]", "Size Limit Warning", "purging", to_remove);
+        this.del(to_remove);
+      }
+      if (!cache[key]) {
+        log("[Cache]", "adding item", key);
+        stack.push(key);
+      } else {
+        log("[Cache]", "updating item", key);
+      }
+      cache[key] = item;
+      return item;
+    },
+
+    get: function(key) {
+      return cache[key] || null;
+    },
+
+    del: function(key) {
+      delete cache[key];
+    },
+
+    clear: function() {
+      cache = {};
+      stack = [];
+      log("[Cache]", "Cache has been sucessfully flushed");
+    },
+
+    size: function() {
+      return stack.length;
+    }
+  };
+
+  return Cache;
+})();
