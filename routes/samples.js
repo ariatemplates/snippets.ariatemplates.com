d454c6a6c54f9f6d258b067a758b60ce673ea30d
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2013-09-27 17:33:10 +0200

    Changing ariatemplates version to latest
    The version to be used by the samples viewer is now the latest available, ie 1.4.10

--- routes/samples.js
+++ routes/samples.js
@@ -47,7 +47,7 @@ var sampleReader = function(app) {
       }
 
       return {
-        'ariatemplates': '1.4.9',
+        'ariatemplates': '1.4.10',
         'sample_id': "sample-"+sample_id,
         'title': yaml.title,
         'yaml': JSON.stringify(yaml),
